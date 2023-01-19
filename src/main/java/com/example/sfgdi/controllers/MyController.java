630a4f4eb4170d677ee8fd5d6033a227e7a13581
Yakodelaragua <araycdd@gmail.com>
2023-01-19 12:42:24 +0100

    primary beans

--- src/main/java/com/example/sfgdi/controllers/MyController.java
+++ src/main/java/com/example/sfgdi/controllers/MyController.java
@@ -1,12 +1,17 @@
 package com.example.sfgdi.controllers;
 
+import com.example.sfgdi.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
-    public String sayHello(){
-        System.out.println("Hello world!!!");
+    private final GreetingService greetingService;
 
-        return "Hi Folks!";
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String sayHello(){
+        return greetingService.sayGreetings();
     }
 }
