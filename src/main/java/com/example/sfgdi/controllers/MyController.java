000cf3799df7158a2d4deb0441b20f62c918b3c8
Yakodelaragua <araycdd@gmail.com>
2023-01-19 11:37:05 +0100

    inicializacion

---
+++ src/main/java/com/example/sfgdi/controllers/MyController.java
@@ -0,0 +1,12 @@
+package com.example.sfgdi.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+    public String sayHello(){
+        System.out.println("Hello world!!!");
+
+        return "Hi Folks!";
+    }
+}
