dec2af5b9092f57308f6177be2156a5743e42a40
Yakodelaragua <araycdd@gmail.com>
2023-01-20 14:30:29 +0100

    Singleton y prototipo

--- src/main/java/com/example/sfgdi/SfgDiApplication.java
+++ src/main/java/com/example/sfgdi/SfgDiApplication.java
@@ -1,6 +1,8 @@
 package com.example.sfgdi;
 
 import com.example.sfgdi.controllers.*;
+import com.example.sfgdi.services.PrototypeBean;
+import com.example.sfgdi.services.SingletonBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -33,6 +35,18 @@ public class SfgDiApplication {
         ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
         System.out.println(constructorInjectedController.getGreeting());
 
+
+        System.out.println("------ Bean Scopes ----------");
+        SingletonBean singletonBean1 = ctx.getBean(SingletonBean.class);
+        System.out.println(singletonBean1.getMyScope());
+
+        SingletonBean singletonBean2 = ctx.getBean(SingletonBean.class);
+        System.out.println(singletonBean2.getMyScope());
+
+        PrototypeBean prototypeBean1 = ctx.getBean(PrototypeBean.class);
+        System.out.println(prototypeBean1.getMyScope());
+        PrototypeBean prototypeBean2 = ctx.getBean(PrototypeBean.class);
+        System.out.println(prototypeBean2.getMyScope());
 
     }
 
---
+++ src/main/java/com/example/sfgdi/services/PrototypeBean.java
@@ -0,0 +1,16 @@
+package com.example.sfgdi.services;
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+@Component
+public class PrototypeBean {
+    public PrototypeBean() {
+        System.out.println("Creating a prototype Bean!!!!!!!!!!!");
+    }
+
+    public String getMyScope(){
+        return "I'm a Prototype";
+    }
+}
